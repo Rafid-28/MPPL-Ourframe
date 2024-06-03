a313ff03a01d5a29bf1a38d07e798b077e75b399
Fahmi <fahmicrew3006@gmail.com>
2024-06-03 13:08:15 +0700

    Adding new Executor

---
+++ EXECUTOR/src/components/pages/Home.js
@@ -0,0 +1,83 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="wireframe-home">
+      <div className="rectangle-3">
+        <div className="child">
+          <img
+            className="child-img"
+            src="/images/Rectangle3.jpeg"
+            alt="Rectangle"
+          />
+        </div>
+        <div className="child">
+          <img
+            className="child-img"
+            src="/images/Rectangle7.jpeg"
+            alt="Rectangle"
+          />
+        </div>
+        <div className="child">
+          <img
+            className="child-img"
+            src="/images/Rectangle82.jpeg"
+            alt="Rectangle"
+          />
+        </div>
+        <div className="child">
+          <img
+            className="child-img"
+            src="/images/Img20240516Wa00011.jpeg"
+            alt="Rectangle"
+          />
+        </div>
+      </div>
+      <div className="container-12">
+        <div className="container">
+          <div className="rectangle-6"></div>
+          <div className="group-2">
+            <div className="group-6">
+              <div className="rectangle-21"></div>
+            </div>
+          </div>
+          <div className="group-22">
+            <div className="rectangle-2"></div>
+          </div>
+          <div className="rectangle-8"></div>
+          <div className="rectangle-7"></div>
+        </div>
+        <div className="group-3">
+          <div className="lorem-ipsum">Lorem Ipsum</div>
+          <span className="sorem-ipsum-dolor-sit-amet-consectetur-adipiscing-elit-etiam-eu-turpis-molestie-dictum-est-amattis-tellus-sed-dignissim-metus-nec-fringilla-accumsan-risus-sem-sollicitudin-lacus-ut-interdum-tellus-elit-sed-risus-maecenas-eget-condimentum-velit-sit-amet-feugiat-lectus-class-aptent-taciti-sociosqu-ad-litora-torquent-per-conubia-nostra-per-inceptos-himenaeos">
+            Sorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+            turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus
+            nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum
+            tellus elit sed risus. Maecenas eget condimentum velit, sit amet
+            feugiat lectus. Class aptent taciti sociosqu ad litora torquent per
+            conubia nostra, per inceptos himenaeos.
+          </span>
+        </div>
+        <div className="container-11">
+          <div className="group-4">
+            <img
+              className="img-child2"
+              src="/images/Rectangle81.jpeg"
+              alt="Rectangle"
+            />
+          </div>
+          <div className="group-4">
+            <img
+              className="img-child2"
+              src="/images/Rectangle8.jpeg"
+              alt="Rectangle"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
